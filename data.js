753416e26a8e70511bf6f8b667fc74c93feb57b6
Angel Lopez <angellopezpol1254@gmail.comm>
2022-05-02 17:37:06 -0400

    Updated Project Card UI, fixed rendering errors

--- data.js
+++ data.js
@@ -2,20 +2,29 @@ export const projects = [
     {
         name: "MeetYourMajor",
         desc: "Skill and interest inventory assessment that college students can take to find their best-fit major",
-        authors: "Angel, Juan, Jorge, and Shehzad",
-        date: "April 20th, 2022"
+        authors: ["Angel", "Juan", "Jorge", "Shehzad"],
+        date: "April 20th, 2022",
+        url: "https://i.imgur.com/Jj0hEs9.jpg",
+        bg: "https://i.imgur.com/DyPu0n0.gif",
+        bgcolor: "pink",
+        index: 0
     },
     {
         name: "Degrees of Separation Finder",
         desc: "Command-line program that implements DFS and Djikstra's Search algorithm to find the minimum degrees of separation between any two nodes in a graph. Utilized on a graph with more than 400,000 nodes and a million edges",
-        authors: "Angel, Jeya, and Jenna",
-        date: "Dont remember, 2021"
+        authors: ["Angel", "Jeya", "Jenna"],
+        date: "Dont remember, 2021",
+        url: "https://i.imgur.com/vKg5Nb7.png",
+        bgcolor: "#31558C",
+        index: 1
     },
     {
         name: "Minesweeper",
         desc: "self-explanatory",
-        authors: "Angel",
-        date: "Dont remember, 2021"
+        authors: ["Angel"],
+        date: "Dont remember, 2021",
+        url: "https://i.imgur.com/qvwoguP.png",
+        index: 2
     },
     {
 
---
+++ githubs.js
@@ -0,0 +1,20 @@
+export const authorgithubs = {
+    Angel: "https://github.com/angel1254mc",
+    Jenna: "https://github.com/jennasheldo",
+    Shehzad: "https://github.com/shehzad02",
+    Jorge: "../",
+    Jeya: "https://github.com/JeyaI",
+    Juan: "https://github.com/juanrodl",
+}
+
+export const projectgithubs = [
+    "https://github.com/juanrodl/cen3031_proj",
+    "https://github.com/angel1254mc/TweetTeam-DegreesOfSeparation",
+    "google.com"
+];
+
+export const projectyoutubes = [
+    "https://www.youtube.com/watch?v=ylNqeFikgsM",
+    "https://www.youtube.com/watch?v=J7PTDxnYY5Y&t=113s",
+    "google.com"
+]
